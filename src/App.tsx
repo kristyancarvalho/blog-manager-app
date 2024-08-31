b6a2cb009c7b7b1c19486ab4aa6e553069db8ff2
Kristyan Carvalho <kristyancarvalho@hotmail.com>
2024-08-31 19:47:38 -0300

    setup init.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen bg-red-600 text-center">
+        <h1 className="text-white font-bold">teste</h1>
+        <Button>receba</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
